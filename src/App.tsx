a7ada72f3b28598fd1eee1a59db92faf9896b009
Niklas Mårdby <niklas.mardby@gmail.com>
2025-04-09 15:00:15 +0200

    Moar stuff

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,6 @@ import ItemDetailsPage from "./routes/ItemDetailsPage";
 import { useState } from "react";
 import { Item } from "./types";
 
-/*
-routes
-
-	startsida med kategorier, klicka på kategori
-	kategori kan vara en dynamisk route :categoryid
-	kategori kan vara statisk men då måste vi uppdatera router här varje gång vi lägger till en kategori
-
-	klicka in på bild bör vara dynamisk eftersom vi lägger till fler bilder
-
-*/
-
 function App() {
 	const [items, setItems] = useState<Item[]>([
 		{
